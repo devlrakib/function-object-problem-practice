a368583b3a7e83afb3558e66dbddd7061b8ef68f
devlrakib <devrakib5@gmail.com>
2024-03-16 16:30:08 +0600

    first commit

---
+++ switch.js
@@ -0,0 +1,34 @@
+// const color = 'green';
+// if (color === 'green'){
+//     console.log('This is Green Color');
+// }
+// else if (color === 'blue'){
+//     console.log('This is blue Color');
+// }
+// else if (color === 'red'){
+//     console.log('This is red Color');
+// }
+// else if (color === 'white'){
+//     console.log('This is white Color');
+// }
+// else if (color === 'Yellow'){
+//     console.log('This is Yellow Color');
+// }
+// else{
+//     console.log('This is Black Color');
+// }
+
+// !Switch
+switch ('green'){
+    case 'green':
+        console.log('You Are A Green Friend');
+        break;
+    case 'blue':
+        console.log('You Are A blue Friend');
+        break;
+    case 'white':
+        console.log('You Are A white Friend');
+        break;
+        default:
+            console.log('You Are A Stranger');
+}
